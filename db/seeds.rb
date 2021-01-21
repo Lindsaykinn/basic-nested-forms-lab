34d9b91e1c7ce83c4493566a5c5985af1a94124d
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-01-21 15:10:18 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+recipes = Recipe.create([
+  {title: "Corn Bread"},
+  {title: "Spaghetti"},
+  {title: "Bread"},
+  {title: "Sauteed Green Beans"},
+  {title: "Creamed Spinach"},
+  {title: "Chilaquiles"}
+])
+
+ingredients = Ingredient.create([
+  {name: "flour", quantity: "1 cup"},
+  {name: "green beans", quantity: "1 cup"},
+  {name: "garlic", quantity: "1 clove"},
+  {name: "spinach", quantity: "5 cups"},
+  {name: "pasta", quantity: "4 servings"}
+])
